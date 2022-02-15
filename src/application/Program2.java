5eceb54af441982578c9cf2e72cdd9dbd2cf6dd5
Levi Freire <levifreire7@gmail.com>
2022-02-15 19:47:32 -0300

    Implementacao do excluir departamento

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -1,5 +1,7 @@
 package application;
 
+import java.util.Scanner;
+
 import model.dao.DaoFactory;
 import model.dao.DepartmentDao;
 import model.entities.Department;
@@ -7,6 +9,8 @@ import model.entities.Department;
 public class Program2 {
 
 	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
 		
 		DepartmentDao departmentDao = DaoFactory.CreateDepartmentDao();
 		
@@ -21,6 +25,14 @@ public class Program2 {
 		dep.setName("Personal");
 		departmentDao.update(dep);
 		System.out.println("Update completed");
+		
+		System.out.println("==== TEST 3: department delete");
+		System.out.println("Enter id for delete departmenrt: ");
+		int id = sc.nextInt();
+		departmentDao.deleteById(id);
+		System.out.println("Delete completed");
+		
+		sc.close();
 
 	}
 
